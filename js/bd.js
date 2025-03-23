b36980aa4c5d11acf5e1fc8994b873c9dab43705
Sofia castelan <sofvelazquez0@gmail.com>
2025-03-23 17:41:13 -0600

    solucionado solo falta responsive

--- js/bd.js
+++ js/bd.js
@@ -1,7 +1,30 @@
-//Forma de almacenar el valor de los botones
-function seleccionarValor(valor) {
+function seleccionarValor(valor, botonSeleccionado) {
     document.getElementById("busca").value = valor;
+    console.log("Valor seleccionado:", valor);
+
+    // Eliminar la clase 'selected' de todos los botones
+    document.querySelectorAll(".btn-servicio").forEach(boton => {
+        boton.classList.remove("selected");
+    });
+
+    // Agregar la clase solo al botón seleccionado
+    botonSeleccionado.classList.add("selected");
 }
+
+// Función para almacenar el valor de los botones de presupuesto
+function seleccionarValorPresupuesto(valor, botonSeleccionado) {
+    document.getElementById("presupuesto").value = valor;
+    console.log("Valor seleccionado:", valor);
+    
+    // Eliminar la clase 'selected' de todos los botones
+    document.querySelectorAll(".pre").forEach(boton => {
+        boton.classList.remove("selected");
+    });
+
+    // Agregar la clase solo al botón seleccionado
+    botonSeleccionado.classList.add("selected");
+}
+
 
 window.onload = function() {
     const urlParams = new URLSearchParams(window.location.search);
