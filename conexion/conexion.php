d14afbaa30ee0ff0580f6c10019acdb21988e863
Sofia castelan <sofvelazquez0@gmail.com>
2025-03-24 21:43:58 -0600

    conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,10 +1,10 @@
 <?php
 class Conexion {
     function ConexionBD() {
-        $host = 'localhost';
-        $dbname = 'portafolio';
-        $username = 'root';
-        $password = '';
+        $host = 'bej91ewkiao6hbnek2z1-mysql.services.clever-cloud.com';
+        $dbname = 'bej91ewkiao6hbnek2z1';
+        $username = 'ucz4kq870ahjn30m';
+        $password = 'Tb5KyBCjOKTOcB3jKwCV';
 
         try {
             $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
